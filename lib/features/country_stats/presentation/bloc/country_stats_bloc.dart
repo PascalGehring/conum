f06a37bb9ac80262b87e59331b01063c244550ec
Pascal Gehring M1 <pascal.gehring@gmx.ch>
2021-03-16 21:34:24 +0100

    completed bloc implementation

--- lib/features/country_stats/presentation/bloc/country_stats_bloc.dart
+++ lib/features/country_stats/presentation/bloc/country_stats_bloc.dart
@@ -1,20 +1,77 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:conum/core/error/failures.dart';
+import 'package:conum/core/usecases/usecase.dart';
+import 'package:conum/core/util/input_converter.dart';
 import 'package:conum/features/country_stats/domain/entities/country_stats.dart';
+import 'package:conum/features/country_stats/domain/usecases/get_concrete_country_stats.dart';
+import 'package:conum/features/country_stats/domain/usecases/get_random_country_stats.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 part 'country_stats_event.dart';
 part 'country_stats_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Das eingegebene Land existiert nicht';
+
 class CountryStatsBloc extends Bloc<CountryStatsEvent, CountryStatsState> {
-  CountryStatsBloc() : super(Empty());
+  final GetConreteCountryStats getConreteCountryStats;
+  final GetRandomCountryStats getRandomCountryStats;
+  final InputConverter inputConverter;
+
+  CountryStatsBloc({
+    @required GetConreteCountryStats concrete,
+    @required GetRandomCountryStats random,
+    @required this.inputConverter,
+  })  : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConreteCountryStats = concrete,
+        getRandomCountryStats = random,
+        super(Empty());
 
   @override
   Stream<CountryStatsState> mapEventToState(
     CountryStatsEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is GetStatsForConcreteCountry) {
+      final inputEither = inputConverter.doesCountryExist(event.countryString);
+
+      yield* inputEither.fold((failure) async* {
+        yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+      }, (string) async* {
+        yield Loading();
+        final failureOrStats =
+            await getConreteCountryStats(Params(country: string));
+        yield* _eitherLoadedOrErrorState(failureOrStats);
+      });
+    } else if (event is GetStatsForRandomCountry) {
+      yield Loading();
+      final failureOrStats = await getRandomCountryStats(NoParams());
+      yield* _eitherLoadedOrErrorState(failureOrStats);
+    }
+  }
+
+  Stream<CountryStatsState> _eitherLoadedOrErrorState(
+      Either<Failure, CountryStats> failureOrStats) async* {
+    yield failureOrStats.fold(
+        (failure) => Error(message: _mapFailureToMessage(failure)),
+        (stats) => Loaded(countryStats: stats));
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }
---
+++ test/features/country_stats/presentation/bloc/country_stats_bloc_test.dart
@@ -0,0 +1,231 @@
+import 'package:conum/core/error/failures.dart';
+import 'package:conum/core/usecases/usecase.dart';
+import 'package:conum/core/util/input_converter.dart';
+import 'package:conum/features/country_stats/domain/entities/country_stats.dart';
+import 'package:conum/features/country_stats/domain/usecases/get_concrete_country_stats.dart';
+import 'package:conum/features/country_stats/domain/usecases/get_random_country_stats.dart';
+import 'package:conum/features/country_stats/presentation/bloc/country_stats_bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockGetConreteCountryStats extends Mock
+    implements GetConreteCountryStats {}
+
+class MockGetRandomCountryStats extends Mock implements GetRandomCountryStats {}
+
+class MockInputConverter extends Mock implements InputConverter {}
+
+void main() {
+  CountryStatsBloc bloc;
+  MockGetConreteCountryStats mockGetConreteCountryStats;
+  MockGetRandomCountryStats mockGetRandomCountryStats;
+  MockInputConverter mockInputConverter;
+
+  setUp(() {
+    mockGetConreteCountryStats = MockGetConreteCountryStats();
+    mockGetRandomCountryStats = MockGetRandomCountryStats();
+    mockInputConverter = MockInputConverter();
+
+    bloc = CountryStatsBloc(
+        concrete: mockGetConreteCountryStats,
+        random: mockGetRandomCountryStats,
+        inputConverter: mockInputConverter);
+  });
+
+  test('initial State should be Empty', () {
+    // assert
+    expect(bloc.state, equals(Empty()));
+  });
+
+  group('GetStatsForConcreteCountry', () {
+    final tCountry = 'Switzerland';
+    final tCountryStats = CountryStats(
+      country: tCountry,
+      population: 800,
+      totalCases: 1,
+      newCases: 1,
+      totalDeaths: 1,
+      newDeaths: 1,
+      criticalPatients: 1,
+    );
+
+    setUpMockInputConverterSucess() {
+      when(mockInputConverter.doesCountryExist(any))
+          .thenReturn(Right(tCountry));
+    }
+
+    test(
+      'should call the InputConverter to validate the user input',
+      () async {
+        setUpMockInputConverterSucess();
+        // act
+        bloc.add(GetStatsForConcreteCountry(tCountry));
+        await untilCalled(mockInputConverter.doesCountryExist(any));
+        // assert
+        verify(mockInputConverter.doesCountryExist(tCountry));
+      },
+    );
+    test(
+      'should emit [Error] when the inputed country does not exist',
+      () async {
+        // arrange
+        when(mockInputConverter.doesCountryExist(any))
+            .thenReturn(Left(InvalidInputFailure()));
+        // assert later
+        final expected = [
+          Error(
+            message: INVALID_INPUT_FAILURE_MESSAGE,
+          )
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForConcreteCountry(tCountry));
+      },
+    );
+
+    test(
+      'should get data from the concrete usecase',
+      () async {
+        // arrange
+        setUpMockInputConverterSucess();
+        when(mockGetConreteCountryStats(any))
+            .thenAnswer((_) async => Right(tCountryStats));
+        // act
+        bloc.add(GetStatsForConcreteCountry(tCountry));
+        await untilCalled(mockGetConreteCountryStats(any));
+        // assert
+        verify(mockGetConreteCountryStats(Params(country: tCountry)));
+      },
+    );
+
+    test(
+      'should emit [Loading], [Loaded] when the data is gotten sucessfully',
+      () async {
+        // arrange
+        setUpMockInputConverterSucess();
+        when(mockGetConreteCountryStats(any))
+            .thenAnswer((_) async => Right(tCountryStats));
+        // assert later
+        final expected = [
+          Loading(),
+          Loaded(countryStats: tCountryStats),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForConcreteCountry(tCountry));
+      },
+    );
+
+    test(
+      'should emit [Loading], [Error] when getting data fails',
+      () async {
+        // arrange
+        setUpMockInputConverterSucess();
+        when(mockGetConreteCountryStats(any))
+            .thenAnswer((_) async => Left(ServerFailure()));
+        // assert later
+        final expected = [
+          Loading(),
+          Error(message: SERVER_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForConcreteCountry(tCountry));
+      },
+    );
+    test(
+      'should emit [Loading], [Error] with a proper Error message when getting data fails',
+      () async {
+        // arrange
+        setUpMockInputConverterSucess();
+        when(mockGetConreteCountryStats(any))
+            .thenAnswer((_) async => Left(CacheFailure()));
+        // assert later
+        final expected = [
+          Loading(),
+          Error(message: CACHE_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForConcreteCountry(tCountry));
+      },
+    );
+  });
+
+  group('GetStatsForRandomCountry', () {
+    final tCountryStats = CountryStats(
+      country: 'Switzerland',
+      population: 800,
+      totalCases: 1,
+      newCases: 1,
+      totalDeaths: 1,
+      newDeaths: 1,
+      criticalPatients: 1,
+    );
+
+    test(
+      'should get data from the random usecase',
+      () async {
+        // arrange
+        when(mockGetRandomCountryStats(any))
+            .thenAnswer((_) async => Right(tCountryStats));
+        // act
+        bloc.add(GetStatsForRandomCountry());
+        await untilCalled(mockGetRandomCountryStats(any));
+        // assert
+        verify(mockGetRandomCountryStats(NoParams()));
+      },
+    );
+
+    test(
+      'should emit [Loading], [Loaded] when the data is gotten sucessfully',
+      () async {
+        // arrange
+        when(mockGetRandomCountryStats(any))
+            .thenAnswer((_) async => Right(tCountryStats));
+        // assert later
+        final expected = [
+          Loading(),
+          Loaded(countryStats: tCountryStats),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForRandomCountry());
+      },
+    );
+
+    test(
+      'should emit [Loading], [Error] when getting data fails',
+      () async {
+        // arrange
+        when(mockGetRandomCountryStats(any))
+            .thenAnswer((_) async => Left(ServerFailure()));
+        // assert later
+        final expected = [
+          Loading(),
+          Error(message: SERVER_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForRandomCountry());
+      },
+    );
+    test(
+      'should emit [Loading], [Error] with a proper Error message when getting data fails',
+      () async {
+        // arrange
+        when(mockGetRandomCountryStats(any))
+            .thenAnswer((_) async => Left(CacheFailure()));
+        // assert later
+        final expected = [
+          Loading(),
+          Error(message: CACHE_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetStatsForRandomCountry());
+      },
+    );
+  });
+}
